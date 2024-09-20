fd8368e2926b06b37e6784fd708a261f54219dad
bjaxqq <brooksieee@proton.me>
2024-09-20 12:27:38 -0400

    Expanded basement level for figuring out mechanics spacing and added a way for the level to be completed

--- src/Maps/Basement.java
+++ src/Maps/Basement.java
@@ -1,5 +1,10 @@
 package Maps;
 
+import java.util.ArrayList;
+
+import EnhancedMapTiles.BasementEndLevelTile;
+import EnhancedMapTiles.BasementEndLevelTile2;
+import Level.EnhancedMapTile;
 import Level.Map;
 import Tilesets.CommonTileset;
 
@@ -8,5 +13,18 @@ public class Basement extends Map {
     public Basement() {
         super("basement.txt", new CommonTileset());
         this.playerStartPosition = getMapTile(2, 13).getLocation();
+    }
+
+    @Override
+    public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
+        ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
+
+        BasementEndLevelTile basementEndLevelTile = new BasementEndLevelTile(getMapTile(68, 2).getLocation());
+        enhancedMapTiles.add(basementEndLevelTile);
+
+        BasementEndLevelTile2 basementEndLevelTile2 = new BasementEndLevelTile2(getMapTile(68, 1).getLocation());
+        enhancedMapTiles.add(basementEndLevelTile2);
+
+        return enhancedMapTiles;
     }
 }
