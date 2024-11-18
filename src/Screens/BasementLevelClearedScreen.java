cb826e70cef9a1ba943ae1ce729ca880bf8628fb
bjaxqq <brooksieee@proton.me>
2024-11-17 22:22:18 -0500

    Began work on cutscenes

--- src/Screens/BasementLevelClearedScreen.java
+++ src/Screens/BasementLevelClearedScreen.java
@@ -1,15 +1,24 @@
 package Screens;
 
 import Engine.GraphicsHandler;
+import Engine.ImageLoader;
 import Engine.Screen;
 import Engine.ScreenManager;
-import SpriteFont.SpriteFont;
+import GameObject.Sprite;
 
 import java.awt.*;
 
 // This class is for the level cleared screen for basement levels
 public class BasementLevelClearedScreen extends Screen {
-    protected SpriteFont winMessage;
+    protected Sprite cutscene = new Sprite(ImageLoader.load("levelTwoCutscene.png"), 0, 0);
+
+    // Fade effect variables
+    protected float fadeValue = 1; // Fully black at start
+    protected boolean isFadingIn = true;
+    protected boolean isFadingOut = false;
+    protected boolean isFadeComplete = false;
+    protected long displayStartTime;
+    protected long displayDuration = 4000; // Extended duration: 4 seconds
 
     public BasementLevelClearedScreen() {
         initialize();
@@ -17,20 +26,50 @@ public class BasementLevelClearedScreen extends Screen {
 
     @Override
     public void initialize() {
-        winMessage = new SpriteFont("Level Complete!", 0, 0, "Times New Roman", 35, Color.white);
+        fadeValue = 1; // Start fully black
+        isFadingIn = true; // Begin with fade-in
+        isFadingOut = false;
+        isFadeComplete = false;
     }
 
     @Override
     public void update() {
-        // Update logic (if needed) goes here
+        // Handle fade-in
+        if (isFadingIn) {
+            fadeValue -= 0.02f; // Gradually reduce opacity
+            if (fadeValue <= 0) {
+                fadeValue = 0;
+                isFadingIn = false;
+                isFadeComplete = true;
+                displayStartTime = System.currentTimeMillis(); // Start timer for display duration
+            }
+        }
+
+        // Handle fade-out after display duration
+        if (isFadeComplete && !isFadingOut) {
+            if (System.currentTimeMillis() - displayStartTime >= displayDuration) {
+                isFadingOut = true;
+            }
+        }
+
+        if (isFadingOut) {
+            fadeValue += 0.02f; // Gradually increase opacity
+            if (fadeValue >= 1) {
+                fadeValue = 1;
+                isFadingOut = false; // Transition complete
+            }
+        }
     }
 
     @Override
     public void draw(GraphicsHandler graphicsHandler) {
-        // Paint entire screen black and display level cleared text
-        graphicsHandler.drawFilledRectangle(0, 0, ScreenManager.getScreenWidth(), ScreenManager.getScreenHeight(), Color.black);
-        winMessage.centerTextX(ScreenManager.getScreenWidth(), graphicsHandler.getGraphics());
-        winMessage.centerTextY(ScreenManager.getScreenHeight(), graphicsHandler.getGraphics());
-        winMessage.draw(graphicsHandler);
+        // Draw the cutscene image
+        cutscene.draw(graphicsHandler);
+
+        // Draw the fade effect
+        if (fadeValue > 0) {
+            Color fadeColor = new Color(0, 0, 0, (int) (fadeValue * 255));
+            graphicsHandler.drawFilledRectangle(0, 0, ScreenManager.getScreenWidth(), ScreenManager.getScreenHeight(), fadeColor);
+        }
     }
 }
--- src/Screens/PlayKitchenLevelScreen.java
+++ src/Screens/PlayKitchenLevelScreen.java
@@ -2,89 +2,51 @@ package Screens;
 
 import Engine.GraphicsHandler;
 import Engine.Screen;
-import Engine.ScreenManager;
 import Engine.Sound;
 import Game.GameState;
 import Game.ScreenCoordinator;
 import Level.Map;
 import Level.Player;
 import Level.PlayerListener;
-import Maps.Basement;
+import Maps.LivingRoom;
 import Players.Spider;
 import Players.WalrusPlayer;
 
-import java.awt.Color;
-
-public class PlayBasementLevelScreen extends Screen implements PlayerListener {
+public class PlayKitchenLevelScreen extends Screen implements PlayerListener {
     protected ScreenCoordinator screenCoordinator;
     protected Map map;
     protected Player player;
     protected PlayLevelScreenState playLevelScreenState;
     protected int screenTimer;
-    protected BasementLevelClearedScreen basementLevelClearedScreen;
-    protected BasementLevelLoseScreen basementLevelLoseScreen;
+    protected KitchenLevelClearedScreen kitchenLevelClearedScreen;
+    protected KitchenLevelLoseScreen kitchenLevelLoseScreen;
     protected boolean levelCompletedStateChangeStart;
 
-    // Fade effect variables
-    protected float fadeValue = 1;
-    protected boolean isFadingIn = true;
-    protected boolean isFadingOut = false;
-    protected boolean isFadeComplete = false;
-
-    public PlayBasementLevelScreen(ScreenCoordinator screenCoordinator) {
+    public PlayKitchenLevelScreen(ScreenCoordinator screenCoordinator) {
         this.screenCoordinator = screenCoordinator;
     }
 
     public void initialize() {
         Sound.stopMusic();
-        this.map = new Basement();
-        if (!screenCoordinator.getArachnophobiaEnabled()) {
+        this.map = new LivingRoom();
+
+        if(!screenCoordinator.getArachnophobiaEnabled()){
             this.player = new Spider(map.getPlayerStartPosition().x, map.getPlayerStartPosition().y);
-        } else {
+        }
+        else{
             this.player = new WalrusPlayer(map.getPlayerStartPosition().x, map.getPlayerStartPosition().y);
         }
-
         this.player.setMap(map);
         this.player.addListener(this);
 
-        basementLevelClearedScreen = new BasementLevelClearedScreen();
-        basementLevelLoseScreen = new BasementLevelLoseScreen(this);
-
-        Sound.playMusic(Sound.Level.BASEMENT);
+        kitchenLevelClearedScreen = new KitchenLevelClearedScreen();
+        kitchenLevelLoseScreen = new KitchenLevelLoseScreen(this);
+        Sound.playMusic(Sound.Level.LIVING_ROOM);
 
         this.playLevelScreenState = PlayLevelScreenState.RUNNING;
-
-        // Initialize fade values
-        fadeValue = 1;
-        isFadingIn = true;
-        isFadingOut = false;
-        isFadeComplete = false;
     }
 
     public void update() {
-        // Handle fade-in and fade-out logic
-        if (isFadingIn) {
-            fadeValue -= 0.02f;
-            if (fadeValue <= 0) {
-                fadeValue = 0;
-                isFadingIn = false;
-                isFadeComplete = true;
-            }
-        }
-
-        if (isFadingOut) {
-            fadeValue += 0.02f;
-            if (fadeValue >= 1) {
-                fadeValue = 1;
-                isFadingOut = false;
-                screenCoordinator.setGameState(GameState.OUTSIDE_LEVEL);
-            }
-        }
-
-        if (!isFadeComplete) {
-            return; // Skip further updates until fade-in is complete
-        }
-
         switch (playLevelScreenState) {
             case RUNNING:
                 player.update();
@@ -95,15 +57,15 @@ public class PlayBasementLevelScreen extends Screen implements PlayerListener {
                     screenTimer = 130;
                     levelCompletedStateChangeStart = false;
                 } else {
-                    basementLevelClearedScreen.update();
+                    kitchenLevelClearedScreen.update();
                     screenTimer--;
                     if (screenTimer == 0) {
-                        isFadingOut = true; // Trigger fade-out when transitioning
+                        goBackToMenu();
                     }
                 }
                 break;
             case LEVEL_LOSE:
-                basementLevelLoseScreen.update();
+                kitchenLevelLoseScreen.update();
                 break;
         }
     }
@@ -115,17 +77,11 @@ public class PlayBasementLevelScreen extends Screen implements PlayerListener {
                 player.draw(graphicsHandler);
                 break;
             case LEVEL_COMPLETED:
-                basementLevelClearedScreen.draw(graphicsHandler);
+                kitchenLevelClearedScreen.draw(graphicsHandler);
                 break;
             case LEVEL_LOSE:
-                basementLevelLoseScreen.draw(graphicsHandler);
+                kitchenLevelLoseScreen.draw(graphicsHandler);
                 break;
-        }
-
-        // Draw fade effect overlay
-        if (fadeValue > 0) {
-            Color fadeColor = new Color(0, 0, 0, (int) (fadeValue * 255));
-            graphicsHandler.drawFilledRectangle(0, 0, ScreenManager.getScreenWidth(), ScreenManager.getScreenHeight(), fadeColor);
         }
     }
 
@@ -154,6 +110,10 @@ public class PlayBasementLevelScreen extends Screen implements PlayerListener {
 
     public void goToOutsideLevelScreen() {
         screenCoordinator.setGameState(GameState.OUTSIDE_LEVEL);
+    }
+
+    public void goToLivingRoomLevelScreen() {
+        screenCoordinator.setGameState(GameState.LIVING_ROOM_LEVEL);
     }
 
     public void goBackToMenu() {
