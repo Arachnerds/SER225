11c2601d4c52de25cbad24dbfd5c236d9ea0674a
bjaxqq <brooksieee@proton.me>
2024-11-18 20:01:00 -0500

    ScreenCoordinator

--- src/Game/ScreenCoordinator.java
+++ src/Game/ScreenCoordinator.java
@@ -41,7 +41,7 @@ public class ScreenCoordinator extends Screen {
 	@Override
 	public void initialize() {
 		// start game off with Menu Screen
-		gameState = GameState.KITCHEN_LEVEL;
+		gameState = GameState.ARACHNERDS;
 	}
 
 	@Override
